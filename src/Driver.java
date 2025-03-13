39708bca4095fdf2d60ceb2a148f46425fb1edb2
Joseph Coakeley <coak0009@algonquinlive.com>
2025-03-13 15:17:15 -0400

    Added javadocs and comment for the following methods in class Group: createBinaryPermutations, managePermutations, commonPermutation, printPermutations, binaryLength, Group. Modified the overlap method to be more compact and only use 1 int array.

--- src/Driver.java
+++ src/Driver.java
@@ -1,15 +1,18 @@
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 
 public class Driver
 {
     public static void main(String[] args)
     {
-        List<Integer> test = new ArrayList<>();
+
+        //Testing inputs to verify correct permutation calculations
+        ArrayList<Integer> test = new ArrayList<>();
         test.add(7);
         test.add(3);
         test.add(5);
         Group test1 = new Group(test, 20);
-        test1.printPermutations();
+        System.out.println(Arrays.toString(test1.Overlap()));
+        //test1.printPermutations();
     }
 }
--- src/Group.java
+++ src/Group.java
@@ -1,16 +1,22 @@
 import java.util.ArrayList;
-import java.util.List;
 import java.util.regex.Pattern;
 
 public class Group
 {
-    List<Integer> clues;
-    List<int[]> binaryPermutations = new ArrayList<>();
-    List<int[]> permutations = new ArrayList<>();
+    ArrayList<Integer> clues;
+    ArrayList<int[]> binaryPermutations = new ArrayList<>();
+    ArrayList<int[]> permutations = new ArrayList<>();
     int size;
 
 
-    public Group(List<Integer> clues, int size)
+    /**
+     * Constructor with parameters that take an ArrayList of clues
+     * and an int for the size of the play field. ie clue: 7, 3, 5
+     * to fit in 20(size) squares.
+     * @param clues ArrayList of Integers that
+     * @param size int The
+     */
+    public Group(ArrayList<Integer> clues, int size)
     {
         this.clues = clues;
         this.size = size;
@@ -18,27 +24,44 @@ public class Group
         createPermutations();
     }
 
-
+    /**
+     * Method that calculates and returns the number of clue elements plus
+     * the extra blank spaces for a given row/column.
+     * @return int Number of clues + number of extra blank spaces.
+     */
     public int binaryLength()
     {
-        int clueSum = 0;
-        for(Integer clueValue : clues)
-            clueSum += clueValue;
+        //Starts the sum at -1 to account for there being 1 less required
+        //space than number of clue number.
+        int clueSum = -1;
 
-        return clues.size() + (size-(clueSum + clues.size()-1));
+        //A loop that adds the values of each clue together plus the 1 space
+        //required between each clue.
+        for(Integer clueValue : clues)
+            clueSum += clueValue + 1;
+
+        return clues.size() + (size-clueSum);
     }
 
-
+    /**
+     * A method that prints to console the binary permutations line by line
+     * followed by the full permutations line by line. Method used to verify
+     * permutations are being generated as intended.
+     */
     public void printPermutations()
     {
+        //Loop for printing each of the binary permutations line by line.
         for(int[] temp:binaryPermutations)
         {
+            //Loop to print out each element of a binary number held in an array.
             for(int i:temp)
                 System.out.print(i);
             System.out.println();
         }
+        //Loop for printing each permutation line by line.
         for(int[] temp:permutations)
         {
+            //Loop for printing out each element of an individual permutation array.
             for(int i:temp)
                 System.out.print(i);
             System.out.println();
@@ -46,15 +69,26 @@ public class Group
     }
 
 
+    /**
+     * A method that compares the same element of each array of the ArrayList
+     * permutations to find any element that may make in all arrays and return
+     * an output array matching the common elements.
+     * @return int[] of current known values in the solution.
+     */
     public int[] commonPermutation()
     {
         int[] output = new int[size];
+        //Assigns the first array of permutations to use to compare with all the
+        //other arrays of permutations.
         int[] primary = permutations.getFirst();
         boolean match;
 
+        //A loop the iterates through each element of the arrays in the
+        //ArrayList permutations to compare their values.
         for(int i=0; i<size; ++i)
         {
             match = true;
+            //A loop for iterating through each array in permutations
             for(int[] secondary:permutations)
             {
                 if(primary[i]!= secondary[i])
@@ -74,30 +108,52 @@ public class Group
         return output;
     }
 
-
+    /**
+     * A method that compares each permutation to the current state of the
+     * solution and removes any permutation that conflicts as they are
+     * no longer a valid permutation.
+     * @param arr The current state of the solution on GameBoard
+     */
     public void managePermutations(int[] arr)
     {
+        //A loop that iterates through each element of arr and takes
+        //action depending on its value.
         for(int i=0; i<arr.length; ++i)
         {
+            //A value of -1 is used to represent a known blank space
+            //A value of 0 is used to represent an unknown square.
             if(arr[i]==-1)
             {
+                //A loop that iterates through each array in the ArrayList
+                //permutations to see if their ith element matches the ith
+                //element of arr.
                 for(int j=0; j<permutations.size(); ++j)
                 {
                     int[] temp = permutations.get(j);
                     if(temp[i] != 0)
                     {
+                        //Removing the permutation that doesn't match with the current
+                        //state of the solution and decrement j as the next permutation
+                        //to check is at the same index as the deleted one.
                         permutations.remove(j);
                         --j;
                     }
                 }
             }
+            //A value of 1 is used to represent a known filled space.
             else if(arr[i]==1)
             {
+                //A loop that iterates through each array in the ArrayList
+                //permutations to see if their ith element matches the ith
+                //element of arr.
                 for(int j=0; j<permutations.size(); ++j)
                 {
                     int[] temp = permutations.get(j);
                     if(temp[i] <= 0)
                     {
+                        //Removing the permutation that doesn't match with the current
+                        //state of the solution and decrement j as the next permutation
+                        //to check is at the same index as the deleted one.
                         permutations.remove(j);
                         --j;
                     }
@@ -147,22 +203,33 @@ public class Group
     }
 
 
+    /**
+     * A method that creates all possible binary numbers of a fixed length
+     * and with a specified number of 1s.
+     */
     public void createBinaryPermutations()
     {
         String num;
-        String regex = "^([^1]*" + "1[^1]*".repeat(clues.size()) + ")$";
+        //Regular expression that will only match with the specified number of 1s
+        String regex = "^(0*" + "10*".repeat(clues.size()) + ")$";
         Pattern pattern = Pattern.compile(regex);
         int length = binaryLength();
+
+        //A loop that generates all integers up to a specified power of 2 and
+        //converts them to binary. If the binary number has the correct number
+        //of 1s leading 0s are added to bring it up to a specified length and
+        //added to the ArrayList binaryPermutations.
         for(int i=0; i<(Math.pow(2, length)); ++i)
         {
             num = Integer.toBinaryString(i);
             if(pattern.matcher(num).matches())
             {
                 num = "0".repeat(length-num.length()) + num;
-
                 int[] temp = new int[num.length()];
+
+                //A loop to convert the binary string into an int[]
                 for(int j=0; j<num.length(); ++j)
-                    temp[j] = Integer.parseInt(String.valueOf(num.charAt(j)));
+                    temp[j] = Character.getNumericValue(num.charAt(j));
 
                 binaryPermutations.add(temp);
             }
@@ -172,65 +239,60 @@ public class Group
 
     public int[] Overlap()
     {
-        int[] output = new int[size];
-        int[] array1 = new int[size];
-        int[] array2 = new int[size];
+        int[] arr = new int[size];
         int index = 0;
         int value = 1;
         int count = 0;
 
-        for(int i=0; i<array1.length; ++i)
+        for(int i = 0; i< arr.length; ++i)
         {
-            if(index< clues.size())
+            if(index<clues.size())
             {
-                if(count< clues.get(index))
+                if(count<clues.get(index))
                 {
-                    array1[i] = value;
+                    arr[i] = value;
                     ++count;
                 }
                 else
                 {
-                    array1[i] = 0;
+                    arr[i] = 0;
                     count = 0;
                     ++index;
                     ++value;
                 }
             }
             else
-                array1[i] = 0;
+                arr[i] = 0;
         }
 
         value = clues.size();
         index = clues.size()-1;
         count = 0;
-        for(int i=array2.length-1; i>-1; --i)
+        for(int i=arr.length-1; i>-1; --i)
         {
             if(index>-1)
             {
-                if(count< clues.get(index))
+                if(count<clues.get(index))
                 {
-                    array2[i] = value;
+                    if(arr[i] == value && arr[i] !=0)
+                    {
+                        arr[i] = 1;
+                    }
+                    else
+                        arr[i] = 0;
                     ++count;
                 }
                 else
                 {
-                    array2[i] = 0;
+                    arr[i] = 0;
                     count = 0;
                     --index;
                     --value;
                 }
             }
             else
-                array2[i] = 0;
+                arr[i] = 0;
         }
-
-        for(int i=0; i<output.length; ++i)
-        {
-            if(array1[i]==array2[i] && array1[i]!=0)
-                output[i] = 1;
-            else
-                output[i] = 0;
-        }
-        return output;
+        return arr;
     }
 }
